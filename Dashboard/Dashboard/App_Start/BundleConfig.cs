6664af77dcea2c35eeac0d74275fe154f262907e
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-08-11 11:11:22 +0200

    BundleTable.EnableOptimizations = true

--- Dashboard/Dashboard/App_Start/BundleConfig.cs
+++ Dashboard/Dashboard/App_Start/BundleConfig.cs
@@ -24,9 +24,9 @@ namespace Dashboard
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+            #if RELEASE 
             BundleTable.EnableOptimizations = true;
+            #endif
         }
     }
 }
