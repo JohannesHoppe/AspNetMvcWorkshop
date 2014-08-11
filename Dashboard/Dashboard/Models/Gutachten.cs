b8f2616d099a89f50a0a1795d34dbb819c78a8d1
lbm1305 <spamcheck@kabelmail.de>
2014-08-11 11:08:51 +0200

    models
    Signed-off-by: JohannesHoppe <johannes.hoppe@haushoppe-its.de>

---
+++ Dashboard/Dashboard/Models/Gutachten.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dashboard.Models
+{
+    public class Gutachten
+    {
+        public int Id { get; set; }
+
+        public DateTime Erstelldatum { get; set; }
+
+        [StringLength(255)]
+        public string Kurztext { get; set; }
+
+        public decimal Kosten { get; set; }
+
+        public Gutachter Gutachter { get; set; }
+    }
+}
---
+++ Dashboard/Dashboard/Models/Gutachter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dashboard.Models
+{
+    public class Gutachter
+    {
+        public Gutachter()
+        {
+            Gutachten = new HashSet<Gutachten>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Vorname { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Nachname { get; set; }
+
+        [EmailAddress( ErrorMessage = "Format der E-Mail Adresse nicht korrekt.")]
+        public string EMail { get; set; }
+
+        public virtual ICollection<Gutachten> Gutachten { get; private set; } 
+    }
+}
