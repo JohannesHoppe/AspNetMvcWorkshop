09f9538a12d20e705d27f9676ab69fa048e86d94
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-08-12 14:17:57 +0200

    no database? no problem!

--- Dashboard/Dashboard/App_Start/ContainerConfig.cs
+++ Dashboard/Dashboard/App_Start/ContainerConfig.cs
@@ -3,6 +3,8 @@ using Autofac;
 using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
 using Dashboard.Models;
+using FakeDbSet;
+using Moq;
 
 namespace Dashboard
 {
@@ -13,14 +15,33 @@ namespace Dashboard
     {
         public static IContainer BuildContainer()
         {
+            // no database? no problem!
+
+            var klaus = new Gutachter { Id = 2, Vorname = "Klaus" };
+            klaus.Gutachten.Add(new Gutachten());
+
+            var inMemoryDbSet = new InMemoryDbSet<Gutachter>(true)
+                             {
+                                 new Gutachter { Id = 1, Vorname = "Hans In Memory" },
+                                 new Gutachter { Id = 2, Vorname = "Horst" },
+                                 new Gutachter { Id = 3, Vorname = "Inge" }
+                             };
+
+            var mockedContext = new Mock<IDashboardContext>();
+            mockedContext.Setup(x => x.Gutachter).Returns(inMemoryDbSet);
+
             var builder = new ContainerBuilder();
 
             builder.RegisterControllers(Assembly.GetExecutingAssembly());
             builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
 
-            builder.RegisterType<DashboardContext>()
-                    .As<IDashboardContext>()
-                    .InstancePerRequest();
+            //builder.RegisterType<DashboardContext>()
+            //        .As<IDashboardContext>()
+            //        .InstancePerRequest();
+
+
+            builder.RegisterInstance(mockedContext.Object)
+                .As<IDashboardContext>();
 
             builder.RegisterType<GutachterRepository>()
                 .As<IGutachterRepository>()
