0cb68a2512db45d6929ad6e333f5ea5b7a06f024
unknown <learnprog@CDS05.hpcc.local>
2014-08-12 09:49:55 +0200

    implemented Repository
    Signed-off-by: JohannesHoppe <johannes.hoppe@haushoppe-its.de>

--- Dashboard/Dashboard/Models/GutachterRepository.cs
+++ Dashboard/Dashboard/Models/GutachterRepository.cs
@@ -1,39 +1,54 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Dashboard.Models
 {
     public class GutachterRepository : IGutachterRepository
     {
 
-
         public GutachterRepository(IDashboardContext context)
         {
-            
+            _context = context;
         }
 
         public int Create(string vorname, string nachname, string mail)
         {
-            throw new System.NotImplementedException();
-        }
-
-        public void Update(Gutachter gutachter)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Gutachter Read(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IEnumerable<Gutachter> ReadAll()
-        {
-            throw new System.NotImplementedException();
+            var newItem = new Gutachter {  EMail = mail, Nachname = nachname, Vorname = vorname };
+            _context.Gutachter.Add(newItem);
+            _context.SaveChanges();
+            return newItem.Id;
         }
 
         public void Delete(int id)
         {
-            throw new System.NotImplementedException();
+            var removableItem = Read(id);
+            _context.Gutachter.Remove(removableItem);
+            _context.SaveChanges();
+        }
+
+        public Gutachter Read(int id)
+        {
+            return _context.Gutachter.SingleOrDefault(x => x.Id.Equals(id));
+        }
+
+        public IEnumerable<Gutachter> ReadAll()
+        {
+            return _context.Gutachter.ToList();
+        }
+
+        public void Update(Gutachter gutachter)
+        {
+            var value = Read(gutachter.Id);
+
+
+
+            if (value != null)
+            {
+                value.EMail = gutachter.EMail;
+                value.Vorname = gutachter.Vorname;
+                value.Nachname = gutachter.Nachname;
+                _context.SaveChanges(); 
+            }
         }
     }
 }
