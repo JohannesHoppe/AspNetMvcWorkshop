8d2ca29353c11dfbac5f65e543894eb9cf6665ae
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-08-12 14:49:02 +0200

    FakedGutachterRepository

--- Dashboard/Dashboard/App_Start/ContainerConfig.cs
+++ Dashboard/Dashboard/App_Start/ContainerConfig.cs
@@ -15,35 +15,12 @@ namespace Dashboard
     {
         public static IContainer BuildContainer()
         {
-            // no database? no problem!
-
-            var klaus = new Gutachter { Id = 2, Vorname = "Klaus" };
-            klaus.Gutachten.Add(new Gutachten());
-
-            var inMemoryDbSet = new InMemoryDbSet<Gutachter>(true)
-                             {
-                                 new Gutachter { Id = 1, Vorname = "Hans In Memory" },
-                                 new Gutachter { Id = 2, Vorname = "Horst" },
-                                 new Gutachter { Id = 3, Vorname = "Inge" }
-                             };
-
-            var mockedContext = new Mock<IDashboardContext>();
-            mockedContext.Setup(x => x.Gutachter).Returns(inMemoryDbSet);
-
             var builder = new ContainerBuilder();
 
             builder.RegisterControllers(Assembly.GetExecutingAssembly());
             builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
 
-            //builder.RegisterType<DashboardContext>()
-            //        .As<IDashboardContext>()
-            //        .InstancePerRequest();
-
-
-            builder.RegisterInstance(mockedContext.Object)
-                .As<IDashboardContext>();
-
-            builder.RegisterType<GutachterRepository>()
+            builder.RegisterType<FakedGutachterRepository>()
                 .As<IGutachterRepository>()
                 .InstancePerRequest();
 
---
+++ Dashboard/Dashboard/Models/FakedGutachterRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoPoco;
+using AutoPoco.DataSources;
+using AutoPoco.Engine;
+
+namespace Dashboard.Models
+{
+    public class FakedGutachterRepository : IGutachterRepository
+    {
+        public static IList<Gutachter> DemoData { get; private set; }
+
+        static FakedGutachterRepository()
+        {
+            DemoData = GenerateDemoData();
+        }
+
+        public int Create(string vorname, string nachname, string mail)
+        {
+            int newId = DemoData.Max(c => c.Id);
+
+            var gutachter = new Gutachter
+                            {
+                                Id = newId,
+                                Vorname = vorname,
+                                Nachname = nachname,
+                                EMail = mail
+                            };
+
+            DemoData.Add(gutachter);
+            return newId;
+        }
+
+        public void Update(Gutachter gutachter)
+        {
+            var gutachterToChagne = DemoData.FirstOrDefault(c => c.Id == gutachter.Id);
+            if (gutachterToChagne != null)
+            {
+                gutachterToChagne.Vorname = gutachterToChagne.Vorname;
+                gutachterToChagne.Nachname = gutachterToChagne.Nachname;
+                gutachterToChagne.EMail = gutachterToChagne.EMail;
+            }
+        }
+
+        public Gutachter Read(int id)
+        {
+            return DemoData.FirstOrDefault(c => c.Id == id);
+        }
+
+        public IEnumerable<Gutachter> ReadAll()
+        {
+            return DemoData;
+        }
+
+        public void Delete(int id)
+        {
+            var gutachter = DemoData.FirstOrDefault(c => c.Id == id);
+            if (gutachter != null)
+            {
+                DemoData.Remove(gutachter);
+            }        
+        }
+
+
+        private static IList<Gutachter> GenerateDemoData()
+        {
+            IGenerationSessionFactory factory = AutoPocoContainer.Configure(x =>
+            {
+                x.Conventions(c => c.UseDefaultConventions());
+                x.AddFromAssemblyContainingType<Gutachter>();
+
+                x.Include<Gutachter>()
+                    .Setup(c => c.Id).Use<IntegerIdSource>()
+                    .Setup(c => c.Vorname).Use<FirstNameSource>()
+                    .Setup(c => c.Nachname).Use<LastNameSource>()
+                    .Setup(c => c.EMail).Use<EmailAddressSource>();
+            });
+
+            IGenerationSession session = factory.CreateSession();
+            IList<Gutachter> gutachter = session.List<Gutachter>(1001).Get();
+            gutachter.RemoveAt(0);
+            return gutachter;
+        }
+    }
+}
