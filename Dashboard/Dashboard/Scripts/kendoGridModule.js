e2d45f19766523d42b7f7099f9ce62a48055266f
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-08-13 10:28:34 +0200

    refactored myFirstModule into two modules, cleaned dependencies! (AMD done right!)

---
+++ Dashboard/Dashboard/Scripts/kendoGridModule.js
@@ -0,0 +1,63 @@
+﻿define(['kendo'], function() {
+
+    var init = function($el) {
+
+        return $el.kendoGrid({
+            dataSource: {
+                type: "odata",
+                transport: {
+                    read: {
+                        url: "/api/Gutachter",
+                        dataType: "json"
+                    }
+                },
+                schema: {
+                    data: function(data) {
+                        return data.value;
+                    },
+                    total: function(data) {
+                        return data['odata.count'];
+                    },
+                    model: {
+                        fields: {
+                            Id: { type: "number" },
+                            Vorname: { type: "string" },
+                            Nachname: { type: "string" },
+                            EMail: { type: "string" }
+                        }
+                    }
+                },
+                pageSize: 20,
+                serverPaging: true,
+                serverFiltering: true,
+                serverSorting: true
+            },
+            height: 550,
+            filterable: true,
+            sortable: true,
+            pageable: true,
+            columns: [
+                {
+                    field: "Id",
+                    filterable: false
+                }, {
+                    field: "Vorname",
+                    title: "Vorname",
+                    filterable: false
+                }, {
+                    field: "Nachname",
+                    title: "Nachname",
+                    filterable: false
+                }, {
+                    field: "EMail",
+                    title: "EMail",
+                    filterable: false
+                }
+            ]
+        }).data("kendoGrid");
+    };
+
+    return {
+        init: init
+    };
+});
--- Dashboard/Dashboard/Scripts/myFirstModule.js
+++ Dashboard/Dashboard/Scripts/myFirstModule.js
@@ -1,71 +1,18 @@
-﻿define(['jquery', 'kendo'], function ($) {
+﻿define(['jquery', 'kendoGridModule'], function ($, kendoGrid) {
+
+// ReSharper disable InconsistentNaming
 
     var _grid;
 
     var _initGrid = function() {
-
-        _grid = $("#grid").kendoGrid({
-            dataSource: {
-                type: "odata",
-                transport: {
-                    read: {
-                        url: "/api/Gutachter",
-                        dataType: "json"
-                    }
-                },
-                schema: {
-                    data: function (data) {
-                        return data.value;
-                    },
-                    total: function (data) {
-                        return data['odata.count'];
-
-                    },
-                    model: {
-                        fields: {
-                            Id: { type: "number" },
-                            Vorname: { type: "string" },
-                            Nachname: { type: "string" },
-                            EMail: { type: "string" }
-                        }
-                    }
-                },
-                pageSize: 20,
-                serverPaging: true,
-                serverFiltering: true,
-                serverSorting: true
-            },
-            height: 550,
-            filterable: true,
-            sortable: true,
-            pageable: true,
-            columns: [
-                {
-                    field: "Id",
-                    filterable: false
-                }, {
-                    field: "Vorname",
-                    title: "Vorname",
-                    filterable: false
-                }, {
-                    field: "Nachname",
-                    title: "Nachname",
-                    filterable: false
-                }, {
-                    field: "EMail",
-                    title: "EMail",
-                    filterable: false
-                }
-            ]
-        }).data("kendoGrid");
+        _grid = kendoGrid.init($("#grid"));
     };
 
     var _getSearch = function() {
-       return $('#search').val();
-    }
+        return $('#search').val();
+    };
 
     var _bindButton = function() {
-
         $('#filterButton').click(function() {
             var dataSource = _grid.dataSource;
 
@@ -79,18 +26,18 @@
                 ]
             });
         });
-
-    }
+    };
 
     var start = function() {
         _initGrid();
         _bindButton();
-    }
+    };
 
     return {
         start: start
     };
 
+    // ReSharper restore InconsistentNaming
 });
 
 
